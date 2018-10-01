34e78ac5e60464c200322647dbb03f873c0c96b8
wovert <wovert@126.com>
2018-10-01 08:34:35 +0800

    array each

--- ES6/dist/index.js
+++ ES6/dist/index.js
@@ -1,49 +1,52 @@
-// ES5 只能在顶部声明严谨模式
 'use strict';
 
-function add(a) {
-  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+// 对象的函数解构 json
 
-  if (a == 0) {
-    throw new Error('A is Error');
-  }
-  return a + b;
-}
-
-function add2(a) {
-  // ES6 可以在此处声明严谨模式
-  'use strict';
-
-  if (a == 0) {
-    throw new Error('A is Error');
-  }
-  return a + b;
-}
-
-// 获得参数参数个数
-function add3(a) {
-  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
-
-  return a + b;
-}
-
-var add4 = function add4(a) {
-  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
-
-  // 此处不能使用new构造函数
-  console.log('wovert');
-  return a + b;
+var json = {
+  a: 'wovert',
+  b: '微明'
 };
-var add5 = function add5(a) {
-  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
-  return a + b;
+function fun(_ref) {
+  var a = _ref.a,
+      _ref$b = _ref.b,
+      b = _ref$b === undefined ? '默认参数' : _ref$b;
+
+  console.log(a, b); // wovert 微明
+}
+fun(json);
+
+// in 用法:是否存在对象属性
+var obj = {
+  a: 'wovert',
+  b: '微明',
+  c: null
 };
+console.log('a' in obj); // true
+console.log('c' in obj); // true
+console.log('d' in obj); // false
 
-// console.log(add(0));
-// console.log(add2(1,2));
+var arr = [, '', null];
+console.log(arr.length); // 3
+console.log(0 in arr); // false
+console.log(1 in arr); // true
+console.log(2 in arr); // true
 
-// add3.length 指向的 没有默认值得形参a
-// console.log(add3(add3.length));
+// 遍历数组
 
-console.log(add4(1, 2));
-console.log(add5(1, 2));
+var n = ['wovert', 'com', 'lingyima'];
+n.forEach(function (value, index) {
+  return console.log(index, value);
+});
+n.filter(function (x) {
+  return console.log(x);
+});
+n.some(function (x) {
+  return console.log(x);
+});
+console.log(n.map(function (x) {
+  return 'web';
+})); // 所有元素替换成web
+
+// 数组转换成字符串
+console.log(n.toString()); // wovert, com, lingyima
+console.log(n.join('|')); // wovert|com|lingyima
--- ES6/src/index.js
+++ ES6/src/index.js
@@ -6,6 +6,34 @@ let json = {
   b: '微明'
 }
 function fun({a,b='默认参数'}) {
-  console.log(a,b);
+  console.log(a,b); // wovert 微明
 }
 fun(json);
+
+// in 用法:是否存在对象属性
+let obj = {
+  a: 'wovert',
+  b: '微明',
+  c: null,
+}
+console.log('a' in obj); // true
+console.log('c' in obj); // true
+console.log('d' in obj); // false
+
+let arr = [,'',null,];
+console.log(arr.length); // 3
+console.log(0 in arr); // false
+console.log(1 in arr); // true
+console.log(2 in arr); // true
+
+// 遍历数组
+
+let n = ['wovert', 'com', 'lingyima'];
+n.forEach((value, index)=> console.log(index,value));
+n.filter(x=> console.log(x));
+n.some(x=> console.log(x));
+console.log(n.map(x=>'web')); // 所有元素替换成web
+
+// 数组转换成字符串
+console.log(n.toString()); // wovert,com,lingyima
+console.log(n.join('|')); // wovert|com|lingyima
